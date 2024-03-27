1c62fd2e6334a25f227092d3605e345383c99dae
Nahid Ahmed <npahmed92@gmail.com>
2024-03-27 02:07:45 -0400

    Project Title of README

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
